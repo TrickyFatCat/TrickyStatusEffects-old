523b0fd0853b96d90d20b7a82b5ef902272f9755
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-02-27 23:34:37 +0200

    Create helper library

---
+++ Source/TrickyStatusEffects/Public/StatusEffectsLibrary.h
@@ -0,0 +1,109 @@
+// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StatusEffectsLibrary.generated.h"
+
+class UStatusEffect;
+class UStatusEffectsManagerComponent;
+class AActor;
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYSTATUSEFFECTS_API UStatusEffectsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static UStatusEffectsManagerComponent* GetStatusEffectsManager(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static UStatusEffect* AddStatusEffect(const TSubclassOf<UStatusEffect> EffectClass,
+	                                      AActor* TargetActor,
+	                                      AActor* Instigator,
+	                                      const int32 StacksAmount);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveAllStatusEffects(AActor* TargetActor, const bool bCustomReason = false);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveAllPositiveStatusEffects(AActor* TargetActor, const bool bCustomReason = false);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveAllNegativeStatusEffects(AActor* TargetActor, const bool bCustomReason = false);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveStatusEffectOfClass(AActor* TargetActor,
+	                                      TSubclassOf<UStatusEffect> EffectClass,
+	                                      const bool bCustomReason = false,
+	                                      const bool bRemoveAllStacks = true,
+	                                      const int32 StacksAmount = 1);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveAllStatusEffectsOfClass(AActor* TargetActor,
+	                                          TSubclassOf<UStatusEffect> EffectClass,
+	                                          const bool bCustomReason = false);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveStatusEffectOfClassByInstigator(AActor* TargetActor,
+	                                                  TSubclassOf<UStatusEffect> EffectClass,
+	                                                  const AActor* Instigator,
+	                                                  const bool bCustomReason = false,
+	                                                  const bool bRemoveAllStacks = true,
+	                                                  const int32 StacksAmount = 1);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveAllStatusEffectsOfClassByInstigator(AActor* TargetActor,
+	                                                      TSubclassOf<UStatusEffect> EffectClass,
+	                                                      const AActor* Instigator,
+	                                                      const bool bCustomReason = false);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool RemoveStatusEffectByObject(AActor* TargetActor,
+	                                       UStatusEffect* StatusEffect,
+	                                       const bool bCustomReason = false,
+	                                       const bool bRemoveAllStacks = true,
+	                                       const int32 StacksAmount = 1);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool GetAllActiveStatusEffects(AActor* TargetActor, TArray<UStatusEffect*>& ActiveEffects);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffectsManager")
+	static bool GetAllPositiveStatusEffects(AActor* TargetActor, TArray<UStatusEffect*>& PositiveEffects) const;
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffectsManager")
+	static bool GetAllNegativeStatusEffects(AActor* TargetActor, TArray<UStatusEffect*>& NegativeEffects) const;
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool HasStatusEffectOfClass(AActor* TargetActor, TSubclassOf<UStatusEffect> EffectClass);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static UStatusEffect* GetStatusEffectOfClass(AActor* TargetActor, TSubclassOf<UStatusEffect> EffectClass);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static bool HasStatusEffectOfClassByInstigator(AActor* TargetActor,
+	                                               TSubclassOf<UStatusEffect> EffectClass,
+	                                               const AActor* Instigator);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	bool GetAllStatusEffectsOfClass(AActor* TargetActor,
+	                                const TSubclassOf<UStatusEffect> EffectClass,
+	                                TArray<UStatusEffect*>& Effects);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	static UStatusEffect* GetStatusEffectOfClassByInstigator(AActor* TargetActor,
+	                                                         TSubclassOf<UStatusEffect> EffectClass,
+	                                                         const AActor* Instigator);
+
+	UFUNCTION(BlueprintCallable, Category="StatusEffects")
+	bool GetAllStatusEffectsOfClassByInstigator(AActor* TargetActor,
+	                                            TSubclassOf<UStatusEffect> EffectClass,
+	                                            const AActor* Instigator,
+	                                            TArray<UStatusEffect*>& Effects);
+};
+
+
